66d154b57ca916033dc1e978a7569a6bdd8e9b96
Abraham Martinez <martinez.abraham@gmail.com>
2022-01-20 13:42:22 -0800

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import app from './server.js'
+import mongodb from "mongodb"
+import dotenv from "dotenv"
+import MoviesDAO from './dao/moviesDAO.js'
+import ReviewsDAO from './dao/reviewsDAO.js'
+
+async function main(){
+    //call dotenv to load the environment variables 
+    dotenv.config()
+
+    const client = new mongodb.MongoClient(
+        process.env.MOVIEREVIEWS_DB_URI
+    )
+    const port = process.env.PORT || 8000
+
+    try {
+        //connect to the mongodb cluster
+        await client.connect()
+        await MoviesDAO.injectDB(client)
+        await ReviewsDAO.injectDB(client)
+
+        app.listen(port, () =>{
+            console.log('server is running on port: ' +port);
+        })
+    } catch (e) {
+        console.error(e);
+        process.exit(1)
+    }
+}
+
+main().catch(console.error);
